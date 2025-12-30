bd654a3232f4d53c462284fb16b5c241e37adba3
saifihaji3765-cmd <saifihaji3765@gmail.com>
2025-12-30 17:11:47 +0530

    Initialize Express server with basic setup

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("🚀 SaaS Backend is Running Successfully");
+});
+
+// Port
+const PORT = process.env.PORT || 5000;
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

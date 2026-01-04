06415eb352055160036eab590401037ee51f0da3
saifihaji3765-cmd <saifihaji3765@gmail.com>
2026-01-04 14:30:59 +0530

    Delete utils/saasGuard.js

--- utils/saasGuard.js
+++
@@ -1,9 +0,0 @@
-export function saasGuard(user) {
-  if (!user) throw new Error("Unauthorized");
-
-  if (user.plan === "free" && user.requests_used >= 3) {
-    throw new Error("Free plan limit reached");
-  }
-
-  return true;
-}
